62d49d3af4a1defdbf234252ce56c86ad4ff97cd
Delt a rashadjamal <rashadjamalansari@gmail.com>
2024-02-01 12:43:23 +0530

    add more component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
+import { Experience } from "./components/Experience/Experience";
+import Works from "./components/Works/Works";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Testimonials from "./components/Testimmonials/Testimonials";
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,10 @@ function App() {
     <Navbar/>
     <Intro/>
     <Services/>
+    <Experience/>
+    <Works/>
+    <Portfolio/>
+    <Testimonials/>
     </div>
   );
 }
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from "../../img/heartemoji.png"
 import Glasses from "../../img/glasses.png"
 import Humble from "../../img/humble.png"
 import Card from '../Card/Card'
+import Resume from './Resume.pdf'
 const Services = () => {
   return (
   <div className='services'>
@@ -16,20 +17,44 @@ const Services = () => {
   <br/>
   ispum is simply dummy text printing
   </span>
-  <button className='button s-button'>Downlod CV</button>
+ <a href={Resume} download>
+
+ <button className='button s-button'>Downlod CV</button>
+ </a>
   <div className='blur  s-blur1' style={{background: "#ABF1FF94"}}></div>
   </div>
     {/* right side */}
     <div className='cards'>
 
-   <div>
+   <div style={{left: '14rem'}}>
     <Card
      emoji ={HeartEmoji}
      heading={'Design'}
-     detail={"Figma , Sketch Photoshop , Adobe ,xd"}
+     detail={"Figma , Sketch Photoshop , Adobe , xd"}
      />
    </div>
- 
+   {/* second card */}
+   <div style={{ top:"12rem", left:"-4rem" }}>
+    <Card
+     emoji ={Glasses}
+     heading={'Developer'}
+     detail={"Html, Css, JavaScript , React"}
+
+     />
+        </div>
+        {/* 3rd */}
+        <div style={{ top:"19rem", left:"12rem" }}>
+    <Card
+     emoji ={Humble}
+     heading={'UI/UX'}
+     detail={"Lorem is simply dumy text of printing of orinting Lorem"
+     }
+
+     />
+        </div>
+        <div className='blur s-blur2' style={{background: "var(--purple)" }}>
+
+        </div>
     </div>
   </div>
   )
